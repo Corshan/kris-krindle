3a1804fdb0f22d524938f5badd2620ddf959dfe2
Corey Shanahan <coreyshanahan01@gmail.com>
2025-02-06 22:38:30 +0000

    add first textbox for names + add & minus buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
+import {MainPageComponent} from './pages/main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot, HeaderComponent],
+  imports: [RouterOutlet, TuiRoot, HeaderComponent, MainPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {TuiAppearance, TuiButton, TuiIcon, TuiTitle} from '@taiga-ui/core';
+import {TuiCardLarge, TuiHeader} from '@taiga-ui/layout';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {TuiInputModule} from '@taiga-ui/legacy';
+import {TuiButtonGroup, TuiIconBadge} from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [
+    TuiCardLarge,
+    TuiAppearance,
+    TuiHeader,
+    TuiTitle,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiButton,
+    TuiIcon,
+    TuiIconBadge,
+    TuiButtonGroup
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
-
+  protected readonly namesForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+    ]),
+  });
 }
