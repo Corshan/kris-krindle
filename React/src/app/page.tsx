a52e4501c3e710371115140552c8330562b3b774
Corey Shanahan <coreyshanahan01@gmail.com>
2025-07-30 15:48:26 +0100

    add cards for names

--- React/src/app/page.tsx
+++ React/src/app/page.tsx
@@ -1,28 +1,56 @@
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import {
+  AppBar,
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{background: "red"}}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon></MenuIcon>
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Kris Krindle
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          position="static"
+          sx={{ background: "red", alignItems: "center" }}
+        >
+          <Toolbar sx={{ gap: 2 }}>
+            <Image src="/santa-claus.png" alt="" width={50} height={50} />
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, fontWeight: "bold" }}
+            >
+              Kris Krindle
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Box
+        sx={{
+          display: "grid",
+          gap: 2,
+          p: 2,
+          gridTemplateColumns: "repeat(2, 1fr)",
+          maxWidth: "90em",
+          mx: 'auto'
+        }}
+      >
+        <Card>
+          <CardHeader title="Add the names" />
+          <CardContent>
+            <TextField id="outlined-basic" label="Name 1" variant="outlined" />
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader title="Who has Who!!" />
+        </Card>
+      </Box>
     </>
   );
 }
